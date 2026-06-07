fbf3e86a6093a3b1f3040d547ebdc0a8d8ed605d
Charan <charan@gmail.com>
2026-06-07 13:06:38 +0530

    'updated'

--- app.py
+++ app.py
@@ -353,8 +353,9 @@ st.markdown("""
     <div class="masthead-overline">📰 Machine Learning · NLP</div>
     <h1 class="masthead-title">BBC News<br>Classifier</h1>
     <p class="masthead-subtitle">
-        Paste any news article and the model will instantly identify which of the five BBC editorial desks it belongs to.
-    </p>
+    Paste any news article and the model will instantly identify<br>
+    which of the five BBC editorial desks it belongs to.
+</p>
     <div class="masthead-rule"></div>
 </div>
 """, unsafe_allow_html=True)
